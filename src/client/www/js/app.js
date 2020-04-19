2ef877ae6bc911ccae20b1e8f742393736935009
jramboasolo <43575202+jramboasolo@users.noreply.github.com>
2020-04-19 14:03:27 +0200

    Initialisation client

---
+++ src/client/www/js/app.js
@@ -0,0 +1,46 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+
+  id: 'io.framework7.myapp', // App bundle ID
+  name: 'FriendFinder', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data() {
+    return {
+      foo: 'bar'
+    };
+  },
+  // App root methods
+  methods: {
+    doSomething() {
+      // ...
+    }
+  },
+
+
+  // App routes
+  routes: routes,
+
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+    },
+  },
+});
