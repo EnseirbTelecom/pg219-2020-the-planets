a4649a2a60c4a0cc89ec3aefa6a0552599eab377
jramboasolo <43575202+jramboasolo@users.noreply.github.com>
2020-04-23 16:19:17 +0200

    Finalisation du formulaire d'inscription et ajout d'un nouveau compte dans Mongodb (mais pas encore de vérification du fait si un autre compte utilise déjà ce mail

--- src/api/server/server.js
+++ src/api/server/server.js
@@ -14,9 +14,30 @@ app.use((req,res,next) => {
 
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
-const urlMongo = "'mongodb://localhost:27017/";
+const urlMongo = "'mongodb://localhost:27017/FriendFinder";
 
-MongoClient.connect(urlMongo){
+MongoClient.connect(urlMongo)
+           .then(clients => clients.db("FriendFinder"))
+           .then(resultat =>{
 
-  app.listen(3000, () => console.log("Awaiting requests."));
-};
+             app.get("/users",(req,res) =>{
+               resultat.collection("users").findOne({"mail": req.body.mail,"password": req.body.password})
+                       .then(item => (item) ? res.json(item) : res.status(404).json({ error: "Entity not found." }))
+                       .catch(err => console.log("err" + err));
+             });
+
+             app.post("/users",(req,res) =>{
+               const product = {
+                 name: req.body.name,
+                 surname: req.body.surname,
+                 mail: req.body.mail,
+                 password: req.body.password
+               };
+               resultat.collection("users").insert(product)
+                       .then(command => res.status(201).json(product));
+             });
+
+
+             app.listen(3000, () => console.log("Awaiting requests."));
+           })
+           .catch(err => { throw err });
